8b85e01abe2310a4a8182e458013cdf13bd8f9c5
Daniel Custódio <srdaniiel@gmail.com>
2015-02-25 17:06:53 -0300

    Inicio do controle de versão com Git
    O projeto foi migrado do SVN code.google.com para o GitHub

---
+++ view/Home/home.php
@@ -0,0 +1,33 @@
+    
+        
+            
+                    <div class="panel panel-default">
+                        <div class="panel-heading">
+                            <i class="fa fa-bar-chart-o fa-fw"></i> Meta da Equipe x Desempenho
+                            <div class="pull-right">
+                                <div class="btn-group">
+                                    <button type="button" class="btn btn-default btn-xs dropdown-toggle" data-toggle="dropdown">
+                                        Menu
+                                        <span class="caret"></span>
+                                    </button>
+                                    <ul class="dropdown-menu pull-right" role="menu">
+                                        <li><a href="#">Imprimir</a>
+                                        </li>
+                                        <li><a href="#">Visualizar</a>
+                                        </li>
+                                        <li class="divider"></li>
+                                        <li><a href="#">Outra Ação</a>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                        <!-- /.panel-heading -->
+                        <div class="panel-body">
+                            <div id="morris-area-chart"></div>
+                        </div>
+                        <!-- /.panel-body -->
+                    </div>
+                    <!-- /.panel -->                   
+                
+        
---
+++ view/template/menuStaticoLateralEsquerda.php
@@ -0,0 +1,98 @@
+<nav class="navbar-default navbar-static-side" role="navigation">
+            <div class="sidebar-collapse">
+                <ul class="nav" id="side-menu">
+                    <!-- Input de Buscas 
+                    <li class="sidebar-search">
+                        <div class="input-group custom-search-form">
+                            <input type="text" class="form-control" placeholder="Search...">
+                            <span class="input-group-btn">
+                                <button class="btn btn-default" type="button">
+                                    <i class="fa fa-search"></i>
+                                </button>
+                            </span>
+                        </div>
+                        <!-- /input-group
+                    </li>
+                     Fim imput buscas--> 
+                    <li>
+                         <!--<a href="index.php"><i class="fa fa-dashboard fa-fw"></i> Informações da Sprint</a>-->
+                        <a href="#"><i class="fa fa-dashboard fa-fw"></i> Acompanhamento<span class="fa arrow"></span></a>
+                        <ul class="nav nav-second-level">
+                            <li>
+                                <a href="#">Projetos</a>
+                                <a href="#sprintsView">Sprints</a>
+                            </li>
+                        </ul>
+                    </li>                 
+
+
+                    <li>
+                        <a href="#"><i class="fa fa-plus fa-fw"></i> Cadastro<span class="fa arrow"></span></a>
+                        <ul class="nav nav-second-level">
+                            <li>
+                                <a href="#projeto"><i class="fa fa-plus fa-fw"></i>Projeto</a>                                
+                            </li>
+
+                            <li>
+                                <a href="#sprint"><i class="fa fa-plus fa-fw"></i>Sprint</a>
+                            </li>
+                            
+                            <li>
+                                <a href="#estoria"><i class="fa fa-plus fa-fw"></i>Estória</a>
+                            </li>
+
+                            <li>
+                                <a href="#time"><i class="fa fa-plus fa-fw"></i>Time</a>
+                            </li>
+                            
+                            <li>
+                                <a href="#papel"><i class="fa fa-plus fa-fw"></i>Papel</a>
+                            </li>
+
+                            <li>
+                                <a href="#tipoTarefa"><i class="fa fa-plus fa-fw"></i>Tipo de Tarefa</a>
+                            </li>
+
+                            <li>
+                                <a href="#tarefa"><i class="fa fa-plus fa-fw"></i>Tarefa</a>
+                            </li>
+                        </ul>
+                    </li>
+                    
+                    
+                    <li>
+                        <a href="#"><i class="fa fa-wrench fa-fw"></i> Configurações<span class="fa arrow"></span></a>
+                        <ul class="nav nav-second-level">
+                            <li>
+                                <a href="#">Segundo Level</a>
+                            </li>
+                            <li>
+                                <a href="#">Segundo Level</a>
+                            </li>
+                            <!-- <li>
+                                <a href="#" ><i class="fa fa-cogs fa-fw"></i> Terceiro Level <span class="fa arrow"></span></a>
+                                <ul class="nav nav-third-level">
+                                    <li>
+                                        <a href="#">Terceiro Level</a>
+                                    </li>
+                                    <li>
+                                        <a href="#">Terceiro Level</a>
+                                    </li>
+                                    <li>
+                                        <a href="#">Terceiro Level</a>
+                                    </li>
+                                    <li>
+                                        <a href="?p=icones">Icones</a>
+                                    </li>
+                                </ul>
+                                
+                            </li> /.nav-third-level -->
+                        </ul>
+                        <!-- /.nav-second-level -->
+                    </li>
+                    
+                </ul>
+                <!-- /#side-menu -->
+            </div>
+            <!-- /.sidebar-collapse -->
+        </nav>
